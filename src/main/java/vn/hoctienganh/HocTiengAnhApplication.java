a1591bff2d6ad8cccd6962f64a8cff77558e590a
daley12306 <thienphuc7434552@gmail.com>
2024-11-21 12:26:06 +0700

    init

---
+++ src/main/java/vn/hoctienganh/HocTiengAnhApplication.java
@@ -0,0 +1,13 @@
+package vn.hoctienganh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HocTiengAnhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HocTiengAnhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/hoctienganh/HocTiengAnhApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hoctienganh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HocTiengAnhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
