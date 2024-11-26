c2ed73e1dd741d5763a43d3ee0119effeb5e76b8
Thien Duy <duythien3624@gmail.com>
2024-11-26 17:18:02 +0700

    merge

---
+++ src/main/java/vn/hoctienganh/controllers/LearnController.java
@@ -0,0 +1,8 @@
+package vn.hoctienganh.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LearnController {
+
+}
--- src/main/java/vn/hoctienganh/entity/User.java
+++ src/main/java/vn/hoctienganh/entity/User.java
@@ -1,5 +1,10 @@
 package vn.hoctienganh.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
 
 }
